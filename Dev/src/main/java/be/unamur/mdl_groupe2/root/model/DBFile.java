49a443a183d4b6770e85d1cafe2056b044928b35
jacquant <antoine.jacques@student.unamur.be>
2019-03-24 16:18:18 +0100

    Application properties update multipart for uploads

--- Dev/src/main/java/be/unamur/mdl_groupe2/root/model/DBFile.java
+++ Dev/src/main/java/be/unamur/mdl_groupe2/root/model/DBFile.java
@@ -1,15 +1,8 @@
 package be.unamur.mdl_groupe2.root.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
 
 @Entity
-@Data
 @Table(name = "files")
 public class DBFile {
     @Id
