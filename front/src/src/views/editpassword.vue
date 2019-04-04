2cd54a06b7282de8fe407d4825b15a46ecc314ac
agossou <agossouema2011@gmail.com>
2019-04-04 20:48:43 +0200

    UPDATE

---
+++ front/src/src/views/editpassword.vue
@@ -0,0 +1,91 @@
+<template>
+    <v-app id="editpassword" class="primary">
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4 lg4>
+                        <v-card class="elevation-1 pa-3">
+                            <v-card-text>
+                                <div class="layout column align-center">
+                                    <img
+                                            src="/static/m.png"
+                                            alt="Vue Material Admin"
+                                            width="120"
+                                            height="120"
+                                    />
+                                    <h1 class="flex my-4 primary--text">Edit your password</h1>
+                                </div>
+                                <v-form>
+
+                                    <v-text-field
+                                            append-icon="lock"
+                                            name="oldpassword"
+                                            label="Old Password"
+                                            id="oldpassword"
+                                            type="password"
+                                            v-model="model.password"
+                                    ></v-text-field>
+                                    <v-text-field
+                                            append-icon="lock"
+                                            name="newpassword"
+                                            label="New Password"
+                                            id="newpassword"
+                                            type="password"
+                                            v-model="model.password"
+                                    ></v-text-field>
+                                    <v-text-field
+                                            append-icon="lock"
+                                            name="newpasswordbis"
+                                            label="Confirm New Password"
+                                            id="newpasswordbis"
+                                            type="password"
+                                            v-model="model.password"
+                                    ></v-text-field>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+
+                                <v-spacer></v-spacer>
+                                <v-btn block color="primary" @click="login" :loading="loading"
+                                >Edit</v-btn
+                                >
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            loading: false,
+            model: {
+                username: "",
+                password: ""
+            }
+        }),
+
+        methods: {
+            login() {
+                this.loading = true;
+                setTimeout(() => {
+                    this.$router.push("/editemail");
+                }, 1000);
+            }
+        }
+    };
+</script>
+<style scoped lang="css">
+    #login {
+        height: 50%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: "";
+        z-index: 0;
+    }
+</style>
---
+++ front/src/src/views/personalpage.vue
@@ -0,0 +1,121 @@
+<template>
+    <div id="page-statistic">
+        <v-container grid-list-xl fluid>
+            <v-layout row wrap>
+                <v-flex sm12>
+                    <h4>Personal Page</h4>
+                </v-flex>
+
+
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    import MiniStatistic from '@/components/widgets/statistic/MiniStatistic';
+    import LinearStatistic from '@/components/widgets/statistic/LinearStatistic';
+    import CircleStatistic from '@/components/widgets/statistic/CircleStatistic';
+    export default {
+        components: {
+            CircleStatistic,
+            MiniStatistic,
+            LinearStatistic
+        },
+        data () {
+            return {
+                trending: [
+                    {
+                        subheading: 'Sales',
+                        headline: '2,55',
+                        caption: 'increase',
+                        percent: 15,
+                        icon: {
+                            label: 'trending_up',
+                            color: 'success'
+                        },
+                        linear: {
+                            value: 15,
+                            color: 'success'
+                        }
+                    },
+                    {
+                        subheading: 'Revenue',
+                        headline: '6,553',
+                        caption: 'increase',
+                        percent: 10,
+                        icon: {
+                            label: 'trending_down',
+                            color: 'error'
+                        },
+                        linear: {
+                            value: 15,
+                            color: 'error'
+                        }
+                    },
+                    {
+                        subheading: 'Orders',
+                        headline: '5,00',
+                        caption: 'increase',
+                        percent: 50,
+                        icon: {
+                            label: 'arrow_upward',
+                            color: 'info'
+                        },
+                        linear: {
+                            value: 50,
+                            color: 'info'
+                        }
+                    }
+                ],
+                trending2: [
+                    {
+                        subheading: 'Email',
+                        headline: '15+',
+                        caption: 'email opens',
+                        percent: 15,
+                        icon: {
+                            label: 'email',
+                            color: 'info'
+                        },
+                        linear: {
+                            value: 15,
+                            color: 'info'
+                        }
+                    },
+                    {
+                        subheading: 'Tasks',
+                        headline: '90%',
+                        caption: 'tasks completed.',
+                        percent: 90,
+                        icon: {
+                            label: 'list',
+                            color: 'primary'
+                        },
+                        linear: {
+                            value: 90,
+                            color: 'success'
+                        }
+                    },
+                    {
+                        subheading: 'Issues',
+                        headline: '100%',
+                        caption: 'issues fixed.',
+                        percent: 100,
+                        icon: {
+                            label: 'bug_report',
+                            color: 'primary'
+                        },
+                        linear: {
+                            value: 100,
+                            color: 'error'
+                        }
+                    },
+                ]
+            };
+        },
+    };
+</script>
+<style lang="stylus" scoped>
+
+</style>
