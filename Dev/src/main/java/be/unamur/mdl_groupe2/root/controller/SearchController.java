41bc35a1f9459817b8ad645a59fc0ad065a0b652
semarbeh <sebastien.marbehant@student.unamur.be>
2019-04-12 15:37:52 +0200

    Mod controller

--- Dev/src/main/java/be/unamur/mdl_groupe2/root/controller/SearchController.java
+++ Dev/src/main/java/be/unamur/mdl_groupe2/root/controller/SearchController.java
@@ -1,6 +1,7 @@
 package be.unamur.mdl_groupe2.root.controller;
 
 import be.unamur.mdl_groupe2.root.model.Article;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/search")
+@RequestMapping("/api")
 public class SearchController {
 
     private final List<Article> searchResult = null;
 
-
+    @PostMapping("QuickSearch")
     public List<Article> searchController(@RequestParam(value="keyword",required = true)String search) {
 
         //TODO
