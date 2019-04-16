92f2cb2615cf89770c9218026d6eeab84215d541
semarbeh <sebastien.marbehant@student.unamur.be>
2019-04-16 14:56:47 +0200

    Del Warnings

--- Dev/src/main/java/be/unamur/mdl_groupe2/root/model/LiteratureReview.java
+++ Dev/src/main/java/be/unamur/mdl_groupe2/root/model/LiteratureReview.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "literatureReviews")
+/*@Table(name = "literatureReviews")*/
 @Data
 /*@Builder*/
 @NoArgsConstructor
