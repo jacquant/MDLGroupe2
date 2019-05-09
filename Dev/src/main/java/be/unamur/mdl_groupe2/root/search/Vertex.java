d8fa6325e0bab050e3b257756fcaba802a157135
semarbeh <sebastien.marbehant@student.unamur.be>
2019-05-09 09:45:25 +0200

    PageRank

---
+++ Dev/src/main/java/be/unamur/mdl_groupe2/root/search/Vertex.java
@@ -0,0 +1,13 @@
+package be.unamur.mdl_groupe2.root.search;
+
+class Vertex {
+    String label;
+    Long score;
+
+    Vertex(String label, Long score) {
+        this.label = label;
+        this.score = score;
+    }
+
+    // equals and hashCode
+}
