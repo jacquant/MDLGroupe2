b0d6a8c2ca45fbc1c30b31a3574652d957a15f9c
semarbeh <sebastien.marbehant@student.unamur.be>
2019-05-12 09:06:53 +0200

    VISU

---
+++ front/src/components/artilceViewGraph.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="popover" id="popover"></div>
+        <div id="container"> </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "artilceViewGraph",
+        mounted(){
+            let graph = document.createElement('script')
+            graph.setAttribute('src', './artilceViewGraph/graph.js')
+            document.head.appendChild(graph)
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
